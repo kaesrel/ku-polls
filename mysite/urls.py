fa8b4aacd07d389113dad822ce69db4c4ff2f755
kaesrel <mr.prom@gmail.com>
2020-09-17 23:39:13 +0700

    Redirect visitors from polls that voting are not allowed or does not exist #20

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.index, name="main_index"),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Choice, Question
 
@@ -17,12 +19,35 @@ class IndexView(generic.ListView):
         published in the future).
         """
         return Question.objects.filter(pub_date__lte=timezone.now()
-                                       ).order_by('-pub_date')[:5]
+                                       ).order_by('-pub_date')
+        # return Question.objects.all()
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    # def get_queryset(self):
+    #     return Question.objects.filter(pub_date__lte=timezone.now())
+
+    def get(self, request, *args, **kwargs):
+        try:
+            question = Question.objects.get(pk=kwargs['pk'])
+            if not question.can_vote():
+                messages.error(request, "That question is not allowed for voting.")
+                return redirect('polls:index')
+        except ObjectDoesNotExist:
+            messages.error(request, "That question does not exist.")
+            return redirect('polls:index')
+        # try:
+        #     self.object = self.get_object()
+        # except Http404:
+        #     # messages.error(request, "That question is not allowed for voting.")
+        #     messages.error(request, kwargs['pk'])
+        #     return redirect('polls:index')
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
 
 
 class ResultsView(generic.DetailView):
@@ -32,6 +57,10 @@ class ResultsView(generic.DetailView):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    # if not question.can_vote():
+    #     # messages.error(request, "This question is not allowed for voting.")
+    #     # return redirect('polls:index')
+    #     return HttpResponseRedirect(reverse('polls:index'), messages.error(request, "This question is not allowed for voting."))
     try:
         selected_choice = \
             question.choice_set.get(pk=request.POST['choice'])
